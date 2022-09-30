4d042205d31e4b7248a8ffd34fb72aa880be40c6
Ding808 <dingyeuyang@gmail.com>
2022-09-30 15:12:41 -0400

    1.1

--- test.py
+++ test.py
@@ -1,25 +1,26 @@
 import main
 n = 0
 for loop in range (0,4):
-  start = input("Do you want to start the game?  Type 'YES' to start")
+  start = input(f"\033[91mDo you want to start the game?  Type 'YES' to start\033[0m")
   if start == "YES":
-    print("START")
+    print("START!!!")
     break
   elif (n == 3):
-    print("DUMB")
+    print(f"\033[91mDUMB\033[0m")
     break
   else:
-    n = n + 1    
+    n = n + 1  
 test1 = main.First()
-test2 = main.Second()
-test3 = main.Third()
 
 test1.show()
-if (test1.pass1()==True):
-    test2.show()
-    if (test2.pass2()==True):
-      test3.show()
-      if (test3.pass3()==True):
-        print("You win the whole game!!!")
+
+# test1.show()
+# if (test1.pass1()==True):
+#     test2.show()
+#     if (test2.pass2()==True):
+#       test3.show()
+#       if (test3.pass3()==True):
+#         print("You win the whole game!!!")
+
 
 
